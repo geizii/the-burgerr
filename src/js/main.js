56a21ab78077d89769c89730b8ae65b83c53433a
geizii <4663072@alunouninter.com>
2024-07-08 18:04:31 -0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+const sidebar = document.querySelector('.sidebar');
+
+function showSideBar() {
+    sidebar.style.display = 'flex';
+    
+}
+
+function hideSideBar() {
+    sidebar.style.display = 'none';
+}
